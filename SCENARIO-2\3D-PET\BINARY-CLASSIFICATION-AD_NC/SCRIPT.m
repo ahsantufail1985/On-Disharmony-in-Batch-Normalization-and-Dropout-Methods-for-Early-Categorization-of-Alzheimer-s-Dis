920859b515fafcbd4716bfff298e425b33dcbcd3
ahsantufail1985 <129931082+ahsantufail1985@users.noreply.github.com>
2023-04-05 11:40:14 +0500

    Create SCRIPT.m

---
+++ SCENARIO-2\3D-PET\BINARY-CLASSIFICATION-AD_NC/SCRIPT.m
@@ -0,0 +1,65 @@
+clc
+clear all
+close all
+filters = 5;
+% DISHARMONY BETWEEN BATCH NORMALIZATION AND DROPOUT CASE-2
+% ADD ONLY ONE DROPOUT LAYER RIGHT BEFORE THE SOFTMAX LAYER
+layers = [...
+    image3dInputLayer([79 95 69],"Name","image3dinput",'Normalization','zerocenter')
+    convolution3dLayer([3 3 3],filters,"Name","conv3d_1","Padding","same",  'WeightL2Factor', 0.00005, 'BiasL2Factor', 0.00005)
+    batchNormalizationLayer("Name","batchnorm_1")
+    eluLayer(1,"Name","elu_1")
+    maxPooling3dLayer([2 2 2],"Name","maxpool3d_1","Padding","same", 'Stride',2)
+    
+    
+    convolution3dLayer([3 3 3],filters,"Name","conv3d_2","Padding","same",'WeightL2Factor', 0.00005, 'BiasL2Factor', 0.00005)
+    batchNormalizationLayer("Name","batchnorm_2")
+    eluLayer(1,"Name","elu_2")
+    maxPooling3dLayer([2 2 2],"Name","maxpool3d_2","Padding","same", 'Stride',2)
+    
+    
+    convolution3dLayer([3 3 3],filters,"Name","conv3d_3","Padding","same",'WeightL2Factor', 0.00005, 'BiasL2Factor', 0.00005)
+    batchNormalizationLayer("Name","batchnorm_3")
+    eluLayer(1,"Name","elu_3")
+    maxPooling3dLayer([2 2 2],"Name","maxpool3d_3","Padding","same", 'Stride',2)
+    
+    
+    convolution3dLayer([3 3 3],filters,"Name","conv3d_4","Padding","same",'WeightL2Factor', 0.00005, 'BiasL2Factor', 0.00005)
+    batchNormalizationLayer("Name","batchnorm_4")
+    eluLayer(1,"Name","elu_4")
+    maxPooling3dLayer([2 2 2],"Name","maxpool3d_4","Padding","same", 'Stride',2)
+    
+    
+    convolution3dLayer([3 3 3],filters,"Name","conv3d_5","Padding","same",'WeightL2Factor', 0.00005, 'BiasL2Factor', 0.00005)
+    batchNormalizationLayer("Name","batchnorm_5")
+    eluLayer(1,"Name","elu_5")
+    maxPooling3dLayer([2 2 2],"Name","maxpool3d_5","Padding","same", 'Stride',2)
+    
+    
+    fullyConnectedLayer(100,"Name","fc_1",'WeightL2Factor', 0.00005, 'BiasL2Factor', 0.00005)
+    fullyConnectedLayer(30,"Name","fc_2",'WeightL2Factor', 0.00005, 'BiasL2Factor', 0.00005)
+    fullyConnectedLayer(2,"Name","fc_3",'WeightL2Factor', 0.00005, 'BiasL2Factor', 0.00005)
+    dropoutLayer(0.1,"Name","dropout")
+    softmaxLayer("Name","softmax")
+    classificationLayer("Name","classoutput")];
+
+TRAINING_SET = imageDatastore('D:\ahsan\TOBECOPIED\temp\FOLD-1\TRAIN', 'IncludeSubfolders',true, 'LabelSource', 'foldernames', 'FileExtensions','.mat','ReadFcn',@(x) matRead(x));
+VALIDATION_SET = imageDatastore('D:\ahsan\TOBECOPIED\temp\FOLD-1\VAL', 'IncludeSubfolders',true, 'LabelSource', 'foldernames', 'FileExtensions','.mat','ReadFcn',@(x) matRead(x));
+TRAINING_SET = shuffle(TRAINING_SET);
+miniBatchSize = 2;
+valFrequency = floor(numel(TRAINING_SET.Files)/miniBatchSize);
+options = trainingOptions('adam', ...
+    'InitialLearnRate',0.001, ...
+    'MaxEpochs',50, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',VALIDATION_SET, ...
+    'LearnRateDropPeriod',10,...
+    'ValidationFrequency',valFrequency, ...
+    'ResetInputNormalization',false, ...
+    'MiniBatchSize',miniBatchSize, ...
+    'LearnRateSchedule','piecewise',...
+    'ExecutionEnvironment','gpu',...
+    'Verbose',false, ...
+    'Plots','training-progress');
+net = trainNetwork(TRAINING_SET,layers,options);
+[Validation_set_predictions,scores] = classify(net,VALIDATION_SET);
